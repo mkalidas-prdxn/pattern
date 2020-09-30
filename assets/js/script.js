fca9d8ccca3bd98726069c677016e39c6a03d56e
kalidasbm <kalidasmahawarkar@gmail.com>
2020-09-30 16:56:16 +0530

    added pattern1 to js  file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,20 @@ function fn1() {
   }
 }
 
+var i, j, k, l, m;
+function pattern1() {
+  document.write("Pattern 1 is : ");
+  document.write('<br><br><br>')
+  for (i = 0; i < n; i++) {
+    for (j = n - i - 1; j > 0; j--) {
+      document.write('&nbsp &nbsp');
+    } document.write('*');
+    for (k = 0; k < i; k++) {
+      document.write(' A *');
+    } document.write("<br>");
+  }
+}
+
 
 
 
