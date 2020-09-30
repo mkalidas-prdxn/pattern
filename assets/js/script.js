d07211b4142ec1b2387561063811b50259f9a763
kalidasbm <kalidasmahawarkar@gmail.com>
2020-09-30 16:58:05 +0530

    added pattern5 to js  file

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,6 +88,40 @@ function pattern4() {
 }
 
 
+function pattern5() {
+  document.write("Pattern 5 is : ");
+  document.write('<br><br><br>')
+  var a = n / 2;
+  var flag = 0;
+  var b = 0;
+  if (n % 2 == 1) {
+    flag = 1;
+    b = -1;
+  }
+  for (i = 0; i < n - a; i++) {
+    for (j = 0; j <= i; j++) {
+      document.write("* &nbsp");
+    }
+    for (k = 0; k < n - a - 1 - i; k++) {
+      document.write("&nbsp &nbsp &nbsp &nbsp &nbsp");
+    }
+    for (j = 0; j <= i; j++) {
+      document.write("&nbsp * ");
+    } document.write("<br>");
+  }
+  for (i = n - a; i > flag; i--) {
+    for (j = i; j > flag; j--) {
+      document.write("* &nbsp");
+    }
+    for (k = n - a - i; k > b; k--) {
+      document.write("&nbsp &nbsp &nbsp &nbsp &nbsp");
+    }
+    for (j = i; j > flag; j--) {
+      document.write("&nbsp * ");
+    } document.write("<br>");
+  }
+}
+
 
 
 
